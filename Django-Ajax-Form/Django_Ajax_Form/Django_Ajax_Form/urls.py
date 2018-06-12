4218163df58eed528238dc4f0fb84b3861385ae8
Nicholas C <codedec116@gmail.com>
2018-06-12 18:20:58 -0400

    set urls

--- Django-Ajax-Form/Django_Ajax_Form/Django_Ajax_Form/urls.py
+++ Django-Ajax-Form/Django_Ajax_Form/Django_Ajax_Form/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from Django_Ajax_Form import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.first_page),
+    url(r'^ajax/calculate/', views.calculate),
+
 ]
